fc28fe7e01e6fec640d1b2f3418852184d742267
GerAone <dmitr.gera.03@mail.ru>
2024-04-21 02:11:44 +0300

    AnVG: Added controller fillGreenFeatureCollectionByBlue;

--- rest-grapher/src/main/java/com/oneune/grapher/store/dto/base/supeclass/AbstractFeatureCollectionDto.java
+++ rest-grapher/src/main/java/com/oneune/grapher/store/dto/base/supeclass/AbstractFeatureCollectionDto.java
@@ -4,10 +4,12 @@ import com.oneune.grapher.store.dto.base.CrsDto;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
 
 
 @NoArgsConstructor
 @AllArgsConstructor
+@SuperBuilder(toBuilder = true)
 @Data
 public abstract class AbstractFeatureCollectionDto {
     private String type;
